316cf3e1136e89a6a977f1096decc6b869a56168
Dakota Bryant <bryant.dakota@gmail.com>
2018-06-15 00:05:49 -0400

    created schemas and resolvers for everything

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+
 import models from './models';
 
-const app = express();
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
 
+const app = express();
+
 const graphqlEndpoint = '/graphql';
-// bodyParser is needed just for POST.
-app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
-app.get('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint })); // if you want GraphiQL enabled
-models.sequelize.sync({ force: true }).then(() => {
+
+app.use(
+  graphqlEndpoint,
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      models,
+      user: {
+        id: 1,
+      },
+    },
+  })
+);
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
+
+models.sequelize.sync({}).then(() => {
   app.listen(8080);
 });
