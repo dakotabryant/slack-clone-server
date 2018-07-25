74ed37461f1c8d999e5083d42c02efacb7cacecf
Dakota Bryant <bryant.dakota@gmail.com>
2018-07-25 16:33:14 -0400

    throwing errors, and doing some db side validation

--- models/user.js
+++ models/user.js
@@ -3,12 +3,30 @@ export default (sequelize, DataTypes) => {
     username: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: 'Username can only be letters and numbers',
+        },
+        len: {
+          args: [3, 25],
+          msg: 'Username needs to be between 3 and 25 characters',
+        },
+      },
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Invalid email',
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
     },
-    password: DataTypes.STRING,
   });
 
   User.associate = (models) => {
--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,12 @@
 import bcrypt from 'bcrypt';
+import { pick } from 'lodash';
+
+const formatErrors = (e, models) => {
+  if (e instanceof models.sequelize.ValidationError) {
+    return e.errors.map((x) => pick(x, ['path', 'message']));
+  }
+  return [{ path: 'name', message: 'something went wrong' }];
+};
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
@@ -7,11 +15,31 @@ export default {
   Mutation: {
     register: async (parent, { password, ...otherArgs }, { models }) => {
       try {
+        if (password.length < 5 || password.length > 100) {
+          return {
+            ok: false,
+            errors: [
+              {
+                path: 'password',
+                message: 'Your passoword must be between 5 and 100 characters.',
+              },
+            ],
+          };
+        }
         const hashedPassword = await bcrypt.hash(password, 12);
-        await models.User.create({ ...otherArgs, password: hashedPassword });
-        return true;
+        const user = await models.User.create({
+          ...otherArgs,
+          password: hashedPassword,
+        });
+        return {
+          ok: true,
+          user,
+        };
       } catch (err) {
-        return false;
+        return {
+          ok: false,
+          errors: formatErrors(err, models),
+        };
       }
     },
   },
